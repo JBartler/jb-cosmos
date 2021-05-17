7f11cadc977f5d69ebf5516fcb858bf82a569c9c
Jakub <bartler.jakub@gmail.com>
2021-05-17 23:33:34 +0200

    prepare files

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import logo from '../../Icons/Logo.png';
+
+import './Navbar.css'
+
+export default function Navbar() {
+    return (
+        <div>
+            <img src={logo} alt='logo icon' />
+        </div>
+    )
+}
---
+++ src/Components/Site/Site.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Navbar from '../Navbar/Navbar';
+
+export default function Site() {
+    return (
+        <div>
+            dupa dupa
+            <Navbar />
+        </div>
+    )
+}
